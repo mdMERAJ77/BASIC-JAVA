164dfa452544875a69a30b6d09bcb02bfd5ad88d
mdMERAJ77 <mmd200975@gmail.com>
2024-01-02 11:21:17 +0530

    Successfully commit

---
+++ Operator/src/ArithemeticOperator.java
@@ -0,0 +1,19 @@
+public class ArithemeticOperator {
+    public static void main(String[] args) {
+        int a=5;
+        int b=2;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);// They give Quotient     Division
+
+        System.out.println(a%b); // Modules Operator to get Remainder in integer division
+
+        System.out.println("When use {double} DataTypes");
+        double c=5;
+        double d=2;
+        System.out.println(c/d);
+        System.out.println(c%d); // Note only use Modules Operator in integer DataTypes
+    }
+}
---
+++ Operator/src/AssignmentOperator.java
@@ -0,0 +1,7 @@
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int A=9;    // Here 9 assign in Variable 'A' Using AssignmentOperator Operator  { = }
+        System.out.println(A);
+    }
+}
