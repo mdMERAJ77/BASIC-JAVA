9e1cb15e95f604452bfb300359118247aee59ff7
mdMERAJ77 <mmd200975@gmail.com>
2024-01-02 14:02:36 +0530

    Successfully commit

--- Pratice Set 2/src/FahrenheitToCelsius.java
+++ Pratice Set 2/src/FahrenheitToCelsius.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class FahrenheitTOCelsius {
+public class FahrenheitToCelsius {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
