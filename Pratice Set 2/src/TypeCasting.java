cec43c719abe5d919761528db4c79ff7cdfaacd0
mdMERAJ77 <mmd200975@gmail.com>
2024-01-03 21:31:22 +0530

    Successfully commit

---
+++ Pratice Set 2/src/TypeCasting.java
@@ -0,0 +1,9 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        int a;
+        float b=123.5425f;
+        System.out.println("Before Casting = "+b);
+        a=(int)b;
+        System.out.println("After Casting = "+a);
+    }
+}
