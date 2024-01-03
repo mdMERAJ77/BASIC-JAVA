28a96033457adc409ed855588dc500a663908da3
mdMERAJ77 <mmd200975@gmail.com>
2024-01-03 10:52:51 +0530

    Successfully Commit

---
+++ Pratice Set 2/src/DeclareVariableAtEnd.java
@@ -0,0 +1,9 @@
+import java.security.MessageDigest;
+
+public class DeclareVariableAtEnd {
+    public static void main(String[] args) {
+        Message="Hello Java Developer";
+        System.out.println(Message);
+    }
+    static String Message;
+}
---
+++ Pratice Set 2/src/LocalConstant.java
@@ -0,0 +1,8 @@
+public class LocalConstant {
+    public static void main(String[] args) {
+        double r=10.0,area;
+        final double Pi=3.14159;
+        area=Pi*r*r;
+        System.out.println("Area Of Circle: "+area);
+    }
+}
