b294cc1071717f92a00ab8f0802c512403ca7f3f
mdMERAJ77 <mmd200975@gmail.com>
2024-01-03 11:16:12 +0530

    Successfully Commit

--- Pratice Set 2/src/SwapUsingThirdVariable.java
+++ Pratice Set 2/src/SwapUsingThirdVariable.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class SwapToNumber
+public class SwapUsingThirdVariable
 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
