f82685e05840b268ff9777d6e4b789c4c11d8b64
mdMERAJ77 <mmd200975@gmail.com>
2024-01-03 21:31:44 +0530

    Successfully Commit

---
+++ Practice Set 3/src/IfExample.java
@@ -0,0 +1,14 @@
+public class IfExample{
+    public static void main(String[] args)
+    {
+        int n=200;
+        if (n>100)
+        {
+            System.out.println("True");
+        }
+        if(n>300)  // This Statement is incorrect, this line will not execute
+        {
+            System.out.println("True");
+        }
+    }
+}
