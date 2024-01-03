a1ba1b06ba39e5269303bf3aef0a030bafe3f758
mdMERAJ77 <mmd200975@gmail.com>
2024-01-03 10:59:01 +0530

    update

--- Pratice Set 2/src/DeclareVariableAtEnd.java
+++ Pratice Set 2/src/DeclareVariableAtEnd.java
@@ -2,8 +2,8 @@ import java.security.MessageDigest;
 
 public class DeclareVariableAtEnd {
     public static void main(String[] args) {
-        Message="Hello Java Developer";
-        System.out.println(Message);
+        Msg="Hello Java Developer";
+        System.out.println("Message: "+Msg);
     }
-    static String Message;
+    static String Msg;
 }
