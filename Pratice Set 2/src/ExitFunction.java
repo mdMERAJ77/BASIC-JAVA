bba90e70923d5608fa166c6fef8ff73cd3ad3f59
mdMERAJ77 <mmd200975@gmail.com>
2024-01-03 11:29:58 +0530

    Successfully Commit

---
+++ Pratice Set 2/src/ExitFunction.java
@@ -0,0 +1,11 @@
+public class ExitFunction {
+    public static void main(String[] args) {
+
+        int a=10;
+        System.out.println("Hello");
+        if(a==10)
+            System.exit(0);    // Exiting the program with status code 0 using System.exit(0)
+        System.out.println("Developer");
+
+    }
+}
