b6ad20750b04c347255314542ae4add0a14ceea3
mdMERAJ77 <mmd200975@gmail.com>
2024-01-04 00:06:20 +0530

    Successfully Commit

---
+++ Operator/src/RelationalOperator.java
@@ -0,0 +1,12 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a=10,b=15;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
+
