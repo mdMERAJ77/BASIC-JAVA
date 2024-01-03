9a2a9156b44d42ebd8546669bdfb5e96ec0a9875
mdMERAJ77 <mmd200975@gmail.com>
2024-01-04 00:35:07 +0530

    Successfully Commit

--- Operator/src/OrderOfOperation.java
+++ Operator/src/OrderOfOperation.java
@@ -6,9 +6,15 @@ public class OrderOfOperation {
         System.out.println(9/(3/3));  // first priority Bracket()
         System.out.println(9/(5-2));
 
-        System.out.println("\n");
+
 
         System.out.println(9/3*6/6);
         System.out.println(9/3*2/6);
+
+
+        int p=4,q=3,r=2;
+        int Sum=p-++r-++q;      // +,- Associativity (left to right)
+        System.out.println("Sum is: "+Sum);
+
     }
 }
