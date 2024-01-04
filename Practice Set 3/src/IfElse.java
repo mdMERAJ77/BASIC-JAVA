be725fbf8b7fe7c8ee8d515898b5721b8a9956eb
mdMERAJ77 <mmd200975@gmail.com>
2024-01-04 16:24:25 +0530

    Successfully Commit

---
+++ Practice Set 3/src/IfElse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class IfElse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcom to Driving License Portal...");
+        System.out.print("Please Enter Your Age:- ");
+
+        int age=sc.nextInt();
+        if(age>=18)
+        {
+            System.out.println("You are Eligible for License");
+        }
+        else
+        {
+            System.out.println("You are not Eligible for License");
+        }
+    }
+}
