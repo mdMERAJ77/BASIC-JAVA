767c797de835dc8f4a0096557fa18528fcda51b3
mdMERAJ77 <mmd200975@gmail.com>
2024-01-04 13:19:58 +0530

    Successfully Commit

---
+++ BitwiseOperator/src/BitwiseSummaryOperator.java
@@ -0,0 +1,11 @@
+public class BitwiseSummaryOperator {
+    public static void main(String[] args) {
+        //SUMMARY:-
+        // (|) Bitwise OR
+        // (&) Bitwise AND
+        // (^) Bitwise exclusive OR Also Known as Bitwise XOR
+        // (~) Bitwise Complement Also Known as Bitwise NOT
+        // (<<) Left Shift
+        // (>>) Right Shift
+    }
+}
