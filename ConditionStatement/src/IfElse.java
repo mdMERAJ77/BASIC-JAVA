86cc00d5bf66e73442400b532d7d03f8b33e7324
mdMERAJ77 <mmd200975@gmail.com>
2024-01-04 15:39:47 +0530

    Successfully Commit

---
+++ ConditionStatement/src/IfElse.java
@@ -0,0 +1,17 @@
+public class IfElse {
+    public static void main(String[] args) {
+        boolean IsMale=false;
+        String Name="Bob";
+
+        System.out.println("After if");
+        if(IsMale)
+        {
+            System.out.println("Mr."+Name);
+        }
+        else
+        {
+            System.out.println("Ms."+Name);
+        }
+        System.out.println("Before if");
+    }
+}
