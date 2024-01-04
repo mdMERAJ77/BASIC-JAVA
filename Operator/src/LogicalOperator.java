08aa5b0454feb0699cd1e6cee916d692142ab40f
mdMERAJ77 <mmd200975@gmail.com>
2024-01-04 12:53:32 +0530

    Successfully Commit

--- Operator/src/LogicalOperator.java
+++ Operator/src/LogicalOperator.java
@@ -3,15 +3,18 @@ public class LogicalOperator {
         int p=15, q=10, r=5;
 
         // && Operator
+        // AND(&&) Operator Both Statement when Correct then Output true(1) Otherwise false(0)
         System.out.println((p>q) && (p>r));   //true
         System.out.println((p>q) && (p<r));   //false
 
         //  || Operator
+        //  OR(||) Operator At least One Statement Correct then Output true(1) Otherwise false(0)
         System.out.println((p>q) || (p<r));   //true
         System.out.println((p<q) || (p<r));   //false
         System.out.println((p<q) || p>r);     //true
 
         //  ! Operator
+        //  NOT(!) Operator Like a Converter. When Statement true(1) Then Output false(0) Similar false(0) Output true(1)
         System.out.println(!(p>q)); //false
         System.out.println((!(p<q))); // true
     }
