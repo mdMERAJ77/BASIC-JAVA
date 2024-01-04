e99116dbe1ce24b0519059832161ad3a160dd10d
mdMERAJ77 <mmd200975@gmail.com>
2024-01-04 12:45:46 +0530

    Successfully Commit

--- Operator/src/AssignmentOperator.java
+++ Operator/src/AssignmentOperator.java
@@ -1,7 +1,7 @@
 
 public class AssignmentOperator {
     public static void main(String[] args) {
-        int A=9;    // Here 9 assign in Variable 'A' Using AssignmentOperator Operator  { = }
+        int A = 9;    // Here 9 assign in Variable 'A' Using AssignmentOperator Operator  { = }
         System.out.println(A);
     }
 }
