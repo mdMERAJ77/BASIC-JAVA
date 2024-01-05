cc57685e32445575c18505dd8f35411c5f3bfed3
mdMERAJ77 <mmd200975@gmail.com>
2024-01-05 15:53:22 +0530

    Successfully Commit

---
+++ Pratice Set 2/src/concatString.java
@@ -0,0 +1,8 @@
+public class concatString {
+    public static void main(String[] args) {
+        String MyFirstName="MD";
+        String MyLastName="MERAJ";
+
+        System.out.println(MyFirstName.concat(MyLastName));
+    }
+}
