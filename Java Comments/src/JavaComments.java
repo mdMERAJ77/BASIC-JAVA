e6e80da6f5c346ef6502c8041a64686caf551950
mdMERAJ77 <mmd200975@gmail.com>
2024-01-05 08:31:35 +0530

    Successfully Commit

---
+++ Java Comments/src/JavaComments.java
@@ -0,0 +1,21 @@
+// Java has three types of Comments
+public class JavaComments
+{
+    /**
+     * System.out.println("Documentation Comment");
+     */
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("This is Single Line Comment");
+        // System.out.println("Single Line Comment");     Welcome
+
+        System.out.println("This is Multi Line Comment");
+       /*
+       System.out.println("Multi Line Comment");
+       Welcome
+        */
+
+    }
+}
