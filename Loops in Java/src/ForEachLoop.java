d7f5075d7e4193bd6a99a7b72f446da7c45e66b6
mdMERAJ77 <mmd200975@gmail.com>
2024-01-06 10:20:53 +0530

    Successfully Commit

--- Loops in Java/src/ForEachLoop.java
+++ Loops in Java/src/ForEachLoop.java
@@ -1,3 +1,20 @@
-public class ForEachLoop {
+public class ForEachLoop
+{
+    public static void main(String[] args)
+        {
+            /* Syntax:-
+            for (DataType variableName : arrayName)
+            {
+                code block to be executed
+            }
+            */
 
+            String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+            for (String i : cars)
+            {
+                System.out.println(i);
+            }
+        }
 }
+
+
