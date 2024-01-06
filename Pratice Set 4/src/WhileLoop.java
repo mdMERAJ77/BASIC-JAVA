9405796fd8d84ad292f56e376486b35cbe8f7c01
mdMERAJ77 <mmd200975@gmail.com>
2024-01-06 12:46:04 +0530

    Successfully Commit

---
+++ Pratice Set 4/src/WhileLoop.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class WhileLoop {
+    public static void main(String[] args) {
+        //Print the sum of the first n natural numbers,where n is the input
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Please Enter the number:- ");
+        int number=sc.nextInt();
+
+        int i=1,sum=0;
+        while(i<=number)
+        {
+            /*
+            sum=i+number;
+              i++;
+             */
+// Here both are correct but:-  sum = sum+=i; better than sum=i+number;
+            sum =sum+ i; // Add i to the sum, not to the number
+            i++;
+
+            System.out.println("sum is:- "+sum);
+            System.out.println("i is:- "+i);
+        }
+        System.out.println("finally sum is "+sum);
+        System.out.println("finally i is "+i);
+    }
+}
