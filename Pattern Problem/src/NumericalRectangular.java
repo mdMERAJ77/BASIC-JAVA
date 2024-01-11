8c920a5eccc6129ed55a5735e2cf450ce23456aa
mdMERAJ77 <mmd200975@gmail.com>
2024-01-11 19:47:27 +0530

    Successfully Commit

--- Pattern Problem/src/NumericalRectangular.java
+++ Pattern Problem/src/NumericalRectangular.java
@@ -1,27 +1,3 @@
-//import java.util.Scanner;
-//public class NumericalRectangular {
-//    public static void main(String[] args) {
-//        Scanner sc=new Scanner(System.in);
-//
-//        System.out.println("Enter the row: ");
-//        int row=sc.nextInt();
-//
-//        for(int i=1;i<=row;i++)
-//        {
-//            for(int j=i;j<=row;j++)
-//            {
-//                System.out.print(j);
-//            }
-//            for(int k=1;k<=i-1;k++)
-//            {
-//                System.out.print(k);
-//            }
-//            System.out.println();
-//        }
-//
-//    }
-//}
-
 import java.util.Scanner;
 public class NumericalRectangular
 {
