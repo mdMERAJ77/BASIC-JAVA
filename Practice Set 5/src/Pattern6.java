c44e2c806ff5560c54f57d1c1c03d7a371f457fd
mdMERAJ77 <mmd200975@gmail.com>
2024-01-12 00:17:44 +0530

    Successfully Commit

--- Practice Set 5/src/Pattern6.java
+++ Practice Set 5/src/Pattern6.java
@@ -10,7 +10,7 @@ public class Pattern6 {
         {
             for(int j=1; j<=number;j++)
             {
-                System.out.print((i*j)%2);
+                System.out.print((i*j)%2+" ");
             }
 
             System.out.println();
--- Practice Set 5/src/Pattern8.java
+++ Practice Set 5/src/Pattern8.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
-public class Pattern7 {
+public class Pattern8 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
         System.out.print("Enter the number: ");
         int number=sc.nextInt();
 
-        for(int i=1;i<=number;i++)
+        for(int i=1; i<=number; i++)
         {
-            for(int j=0;j<number;j++)
+            for(int j=0; j<number; j++)
             {
-                System.out.print((i*j)%2+" ");
+                System.out.print((j%2)+" ");
             }
             System.out.println();
         }
