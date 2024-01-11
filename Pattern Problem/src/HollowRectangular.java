03856fcef31b968bab232fa886bda2a2b4adee76
mdMERAJ77 <mmd200975@gmail.com>
2024-01-11 16:52:39 +0530

    Successfully Commit

--- Pattern Problem/src/HollowRectangular.java
+++ Pattern Problem/src/HollowRectangular.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
 public class HollowRectangular {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the row: ");
+        int row=sc.nextInt();
+        System.out.print("Enter the column: ");
+        int column=sc.nextInt();
+
+        for(int i=1;i<=row;i++)
+        {
+            for(int j=1;j<=column;j++)
+            {
+                if(i == 1 || i == row || j == 1 || j==column)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
 }
