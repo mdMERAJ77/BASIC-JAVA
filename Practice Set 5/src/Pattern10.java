5462931cc9c7caa44c76d79271ac0b8dfa54a22e
mdMERAJ77 <mmd200975@gmail.com>
2024-01-12 10:15:15 +0530

    Successfully Commit

---
+++ Practice Set 5/src/Pattern10.java
@@ -0,0 +1,13 @@
+public class Pattern10 {
+    public static void main(String[] args) {
+
+        for(char i='A';i<='F';i++)
+        {
+            for(char j='A';j<='F';j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Practice Set 5/src/Pattern9.java
@@ -0,0 +1,13 @@
+public class Pattern9 {
+    public static void main(String[] args) {
+
+        for(char i='A';i<='F';i++)
+        {
+            for(char j='A';j<='F';j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
