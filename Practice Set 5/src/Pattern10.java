99217e2e0ca6ddcb284d2ef1099d6e79102d8895
mdMERAJ77 <mmd200975@gmail.com>
2024-01-12 15:17:05 +0530

    Successfully Commit

--- Practice Set 5/src/Pattern10.java
+++ Practice Set 5/src/Pattern10.java
@@ -1,13 +1,29 @@
-public class Pattern10 {
+import java.util.Scanner;
+public class Pattern10
+{
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
 
-        for(char i='A';i<='F';i++)
+        System.out.print("Enter the row: ");
+        int row=sc.nextInt();
+        System.out.print("Enter the column: ");
+        int column=sc.nextInt();
+
+        for(int i=1;i<=row;i++)
         {
-            for(char j='A';j<='F';j++)
+            for(int j=1;j<=column;j++)
             {
-                System.out.print(j+" ");
+                if((i+j)%2==0)
+                {
+                    System.out.print("1");
+                }
+                else
+                {
+                    System.out.print("2");
+                }
             }
             System.out.println();
         }
+
     }
 }
