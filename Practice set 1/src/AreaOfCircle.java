48068167e77b50a9b2cd265fd6a25bbbc0c6c838
MD MERAJ <mmd200975@gmail.com>
2024-01-14 12:34:31 -0800

    Successfully Commit

--- Practice set 1/src/AreaOfCircle.java
+++ Practice set 1/src/AreaOfCircle.java
@@ -3,11 +3,11 @@ public class AreaOfCircle {
     public static void main(String[] args) {
         System.out.println("Calculate The Area of Circle...");
 
-        System.out.println("Enter the Radius1");
+        System.out.println("Enter the Radius1: ");
         Scanner sc=new Scanner(System.in);
         float radius1=sc.nextInt();
 
-        System.out.println("Enter the Radius2");
+        System.out.println("Enter the Radius2: ");
         float radius2=sc.nextInt();
 
         double Area=3.14*radius1*radius2;
