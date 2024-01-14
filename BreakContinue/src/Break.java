940f693173886bf08efe88f0c4c82c8ab4bd7cf5
MD MERAJ <mmd200975@gmail.com>
2024-01-14 12:36:17 -0800

    Successfully Commit

--- BreakContinue/src/Break.java
+++ BreakContinue/src/Break.java
@@ -2,7 +2,7 @@ public class Break {
     public static void main(String[] args) {
 
         int i;
-        for(i=1;i<=10;i++)
+        for(i=1; i<=10; i++)
         {
             if(i == 5)
             {
