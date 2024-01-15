2504caee91d696d471afe85b334f4885784b3a60
MD MERAJ <mmd200975@gmail.com>
2024-01-16 05:20:10 +0530

    Successfully Commit

--- NumberSystem/src/BinaryToDecimal.java
+++ NumberSystem/src/BinaryToDecimal.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 public class BinaryToDecimal {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the binary_number: ");
+        int binary_number=sc.nextInt();
+
+        int ans=0;
+        int power=1;
+        while(binary_number>0)
+        {
+            ans+=(power*(binary_number%10));
+            binary_number/=10;
+            power=power*2;
+        }
+        System.out.println("Decimal number is: "+ans);
+
     }
 }
