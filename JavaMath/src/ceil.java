c7c2ca564eb333e4564538268cf29f743a0f540e
MD MERAJ <mmd200975@gmail.com>
2024-01-16 12:13:16 +0530

    Successfully Commit

---
+++ JavaMath/src/ceil.java
@@ -0,0 +1,8 @@
+public class ceil {
+    public static void main(String[] args) {
+        double a=Math.ceil(5.2);
+        double b=Math.ceil(5.9);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
