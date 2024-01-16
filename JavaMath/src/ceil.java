e1abc3cad57517d3324f4e76a7eb248282545ad0
MD MERAJ <mmd200975@gmail.com>
2024-01-16 12:15:37 +0530

    Successfully Commit

--- JavaMath/src/ceil.java
+++ JavaMath/src/ceil.java
@@ -1,7 +1,7 @@
 public class ceil {
     public static void main(String[] args) {
-        double a=Math.ceil(5.2);
-        double b=Math.ceil(5.9);
+        double a=Math.ceil(5.2);    // output 6.0
+        double b=Math.ceil(5.9);    // output 6.0
         System.out.println(a);
         System.out.println(b);
     }
--- JavaMath/src/floor.java
+++ JavaMath/src/floor.java
@@ -1,7 +1,7 @@
 public class floor {
     public static void main(String[] args) {
-        double a=Math.floor(5.2);
-        double b=Math.floor(5.9);
+        double a=Math.floor(5.2);   // output 5.0
+        double b=Math.floor(5.9);   // output 5.0
 
         System.out.println(a);
         System.out.println(b);
