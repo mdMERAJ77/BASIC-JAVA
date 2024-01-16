a178939738c1f8b8113e2d267302d2562c1463c8
MD MERAJ <mmd200975@gmail.com>
2024-01-16 12:11:58 +0530

    Successfully Commit

---
+++ JavaMath/src/floor.java
@@ -0,0 +1,9 @@
+public class floor {
+    public static void main(String[] args) {
+        double a=Math.floor(5.2);
+        double b=Math.floor(5.9);
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
