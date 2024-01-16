ec63a34bc42efe46445950258b86a687c09f7cac
MD MERAJ <mmd200975@gmail.com>
2024-01-16 07:15:57 +0530

    Successfully Commit

---
+++ OOPsConcept/Classes And Objects/src/Person.java
@@ -0,0 +1,15 @@
+// The Person class represents a simple entity with an age property.
+
+public class Person {
+    // The age property of the Person, initialized to 20 by default.
+    int age = 20;
+
+    // The main method is the entry point of the program.
+    public static void main(String[] args) {
+        // Create an instance of the Person class named Meraj.
+        Person Meraj = new Person();
+
+        // Print the age of the Person instance (Meraj) to the console.
+        System.out.println("Meraj's age: " + Meraj.age);
+    }
+}
