36f7c1cfdb3addbe5d05d399c7a8105981900d5e
MD MERAJ <mmd200975@gmail.com>
2024-01-18 06:10:47 +0530

    Successfully Commit

---
+++ Strings/src/EscapeSequenceCharacters.java
@@ -0,0 +1,14 @@
+public class EscapeSequenceCharacters {
+    public static void main(String[] args) {
+
+        System.out.println("I am escape sequence \"double quote\". ");
+
+        System.out.println("Hello\nBoys");
+
+        System.out.println("HELLO \\ HI");
+
+        System.out.println("HELLO\tTHANKS");
+
+        System.out.println("MERAJ\f\fSAKIB");
+    }
+}
