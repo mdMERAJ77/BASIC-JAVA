30e113a11d18000eba4d6c89fad79372502c4541
MD MERAJ <mmd200975@gmail.com>
2024-01-18 21:04:08 +0530

    Successfully Commit

--- Arrays/src/Class2.java
+++ Arrays/src/Class2.java
@@ -2,7 +2,15 @@ public class Class2 {
     public static void main(String[] args)
     {
         int[] marks={101,102,103,104};
-        System.out.println(marks[1]); //OUTPUT 101
-        System.out.println(marks.length);
+        //System.out.println(marks[1]); //OUTPUT 101
+
+        /*for(int i=0;i<marks.length;i++){
+            System.out.println(marks[i]);
+         */
+        for (int element:marks)
+        {
+            System.out.println(element);
+        }
+
     }
 }
