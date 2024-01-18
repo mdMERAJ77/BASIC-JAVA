7aed386db8ca0a4b6903fc5d2206068c7bd4f51b
MD MERAJ <mmd200975@gmail.com>
2024-01-18 05:54:16 +0530

    Successfully Commit

---
+++ Strings/src/equals.java
@@ -0,0 +1,8 @@
+public class equals {
+    public static void main(String[] args) {
+        String name="MERAJ";
+
+        System.out.println(name.equals("MERAJ")); // OUTPUT true
+        System.out.println(name.equals("meraj")); // OUTPUT false
+    }
+}
---
+++ Strings/src/lastIndexOf.java
@@ -0,0 +1,8 @@
+public class lastIndexOf {
+    public static void main(String[] args) {
+        String name="MERAJMERAJ";
+
+        System.out.println(name.lastIndexOf("M"));  // OUTPUT 5
+        System.out.println(name.lastIndexOf("m"));  // OUTPUT -1 ("m" not found return -1)
+    }
+}
