697bcb3be164f60b6c8134afa6dbfb6182307454
MD MERAJ <mmd200975@gmail.com>
2024-01-18 05:58:52 +0530

    Successfully Commit

---
+++ Strings/src/equalsIgnoreCase.java
@@ -0,0 +1,8 @@
+public class equalsIgnoreCase {
+    public static void main(String[] args) {
+        String name="MERAJ";
+
+        System.out.println(name.equalsIgnoreCase("meraj")); //OUTPUT true
+        System.out.println(name.equalsIgnoreCase("MERAJ")); //OUTPUT false
+    }
+}
