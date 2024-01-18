1fc554dc399265de2426554a234728097c222249
MD MERAJ <mmd200975@gmail.com>
2024-01-18 06:50:22 +0530

    Successfully Commit

---
+++ StringPraticeSet/src/problem1.java
@@ -0,0 +1,8 @@
+// write a java program to replace spaces with underscore
+public class problem1 {
+    public static void main(String[] args)
+    {
+        String text="Hello java developer";
+        System.out.println(text.replace("e","_")); //OUTPUT H_llo java d_v_lop_r
+    }
+}
---
+++ StringPraticeSet/src/toLowerCase.java
@@ -0,0 +1,6 @@
+public class toLowerCase {
+    public static void main(String[] args) {
+        String name="MERAJ";
+        System.out.println(name.toLowerCase());
+    }
+}
